6eec904d45c740df6d13bf8c438face246c08fb3
Wes Kelly <weskelly@mac.com>
2016-01-25 12:43:51 -0500

    Initial push, empty project

---
+++ TokenScanner/src/tokenscanner/TokenScanner.java
@@ -0,0 +1,31 @@
+
+/**
+ * @author Wesley Kelly
+ * @version 1.0
+ *
+ * File: __NAME__.java Created: __DATE__, __TIME__
+ *
+ * Copyright {YEAR!!!} Cedarville University, its Computer Science faculty, and
+ * the authors. All rights reserved.
+ *
+ * Description:
+ */
+
+package tokenscanner;
+
+/**
+ *
+ * @author wes
+ */
+public class TokenScanner
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
