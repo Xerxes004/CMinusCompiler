910dea1835a7f55e0a0302f82ed45fdc8959a103
Wes Kelly <weskelly@mac.com>
2016-02-19 13:23:18 -0500

    Update tests for new file structure.

--- TokenScanner/src/tokenscanner/Test.java
+++ TokenScanner/src/tokenscanner/Test.java
@@ -1,48 +1,70 @@
+/**
+ * @author Johnathan Coraccio, Jimmy Von Eiff, Wesley Kelly
+ * @version 1.0
+ *
+ * File: Test.java 
+ * Created: 12 February, 2016
+ *
+ * Copyright 2016 Cedarville University, its Computer Science faculty, and the 
+ * authors. All rights reserved.
+ *
+ * Description: This class is used to test the JFlex_CMinusScanner class
+ */
+
+
 package tokenscanner;
 
 import java.io.*;
 
-/**
- * Compiler Theory & Practice
- * Dr. Gallagher
- * Project 1
- * Test.java
- * @author Johnathan Coraccio
- * Date: February 12, 2016
- */
-public class Test {
-    
-    public static void main(String [] args){
-        String fileName = "C:\\Users\\Racci\\Source\\Repos\\Compiler\\TokenScanner\\src\\tokenscanner\\exceptionTest.txt";
-        String saveName = "C:\\Users\\Racci\\Source\\Repos\\Compiler\\TokenScanner\\src\\tokenscanner\\JFLEXoutput.txt";
-        
-        scan(fileName, saveName);
-    }
-    
-    public static void scan(String fileName, String saveName){
+public class Test
+{
+    public static void scan(String fileName, String saveName)
+    {
         BufferedReader br = null;
         BufferedWriter bw = null;
-        try{
+        
+        try
+        {
             br = new BufferedReader(new FileReader(fileName));
             bw = new BufferedWriter(new FileWriter(saveName));
+            
             JFlex_CMinusScanner scanner = new JFlex_CMinusScanner(br);
-            while(scanner.peekNextToken().getType() != Token.TokenType.EOF){
+            
+            while (scanner.peekNextToken().getType() != Token.TokenType.EOF)
+            {
                 Token tok = scanner.getNextToken();
-                bw.write(String.format("%s\t\t\t%s\n", tok.getType(), tok.getData()));
+                bw.write(String.format("%20.20s\t%s\n", tok.getType(), tok.getData()));
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println("File not found " + ex);
-        } catch (IOException ex){
-            System.out.println("Error while opening file " + ex);
-        } finally {
-            try{
-                if(bw != null){
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("File not found " + ex.getMessage());
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Error while opening file " + ex.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (bw != null)
+                {
                     bw.close();
                 }
             }
-            catch(IOException ex){
+            catch (IOException ex)
+            {
                 System.out.println("Error while closing file: " + ex);
             }
         }
+    }
+
+    public static void main(String[] args)
+    {
+        String fileName = "./test/inputs/test.txt";
+        String saveName = "./test/outputs/test.out";
+
+        scan(fileName, saveName);
     }
 }
