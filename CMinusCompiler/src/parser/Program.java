707f92ceca1099aba3a43ccaea03b53351581512
Wes Kelly <weskelly@mac.com>
2016-03-21 16:29:26 -0400

    Add Program class.

---
+++ CMinusCompiler/src/parser/Program.java
@@ -0,0 +1,25 @@
+/*
+ * 
+ * @author Wesley Kelly
+ * @version 1.0
+ *
+ * File: GameBoard.java Created: 27 October 2015
+ *
+ * Copyright Cedarville University, its Computer Science faculty, and the
+ * authors. All rights reserved.
+ *
+ * Description: 
+ */
+
+package parser;
+
+import java.util.ArrayList;
+
+public class Program {
+    public Program()
+    {
+        declarations = new ArrayList<>();
+    }
+    
+    private ArrayList<Declaration> declarations;
+}
